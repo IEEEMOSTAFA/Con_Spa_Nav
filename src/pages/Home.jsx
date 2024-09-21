8fd9cd8c8726fa0d46ddb0bd1bbb5a83e3ad8382
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-09-22 00:03:00 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Nav from "../components/Nav";
+
+const Home = () => {
+    return (
+        <div>
+            {/* {Navbar} */}
+            <Nav></Nav>
+            <h1>Home Page</h1>
+        </div>
+    );
+};
+
+export default Home;
